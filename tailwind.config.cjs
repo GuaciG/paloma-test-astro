d115ecf0714f3ba878d17d52664e56f3c367a04f
Guaci Gutierrez <guacig@hotmail.com>
2023-07-18 22:34:09 +0100

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['Anton, sans-serif']
+    },
+    extend: {
+      rotate: {
+        270: '270deg'
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/public/paloma-infinity.png')"
+      }
+    }
+  },
+  plugins: []
+}
